74135633071e44ccf9e64a4d0436a0dc10e7393e
minu <made_power@naver.com>
2023-05-30 23:23:08 +0900

    CRUD 작업 테스트
    CRUD 로직을 생성 후 컨트롤러, 서비스 계층에 관하여 테스트가 완료된 상태이다.

--- src/test/java/com/example/toyblog/controller/PostControllerTest.java
+++ src/test/java/com/example/toyblog/controller/PostControllerTest.java
@@ -2,21 +2,27 @@ package com.example.toyblog.controller;
 
 import com.example.toyblog.domain.Post;
 import com.example.toyblog.dto.request.CreatePost;
+import com.example.toyblog.dto.request.EditPost;
 import com.example.toyblog.repository.PostRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -37,7 +43,7 @@ class PostControllerTest {
     void test1() throws Exception {
      mockMvc.perform(get("/posts"))
              .andExpect(status().isOk())
-             .andExpect(content().string("hell"))
+             //.andExpect(content().string("hell"))
              .andDo(print());
     }
 
@@ -106,5 +112,73 @@ class PostControllerTest {
                 .andDo(print());
     }
 
+    @Test
+    @DisplayName("글 여러 개 조회")
+    void 글_여러개_조회() throws Exception {
+        //given
+        Post post1 = new Post("제목1","내용1");
+        Post post2 = new Post("제목2","내용2");
+        postRepository.save(post1);
+        postRepository.save(post2);
 
+
+        mockMvc.perform(get("/posts")
+                        .contentType(APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(2)))
+                .andExpect(jsonPath("$[0].id").value(post1.getId()))
+                .andExpect(jsonPath("$[0].title").value(post1.getTitle()))
+                .andExpect(jsonPath("$[0].content").value("내용1"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("페이징 처리 결과 조회")
+    void 글_여러개_페이징_조회() throws Exception {
+        //given
+        List<Post> requestPosts = IntStream.range(1,31)
+                .mapToObj(i-> new Post("제목"+i,"내용"+i))
+                .collect(Collectors.toList());
+        postRepository.saveAll(requestPosts);
+
+
+        mockMvc.perform(get("/posts?page=1&size=10&sort=id,desc")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()",is(10)))
+                .andExpect(jsonPath("$[0].id").value(30))
+                .andExpect(jsonPath("$[0].title").value("제목30"))
+                .andExpect(jsonPath("$[0].content").value("내용30"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("글 제목 수정")
+    void 글_제목_수정() throws Exception {
+        //given
+        Post post = new Post("제목1", "수정1");
+        postRepository.save(post);
+
+        EditPost editPost = new EditPost("바뀐 제목입니다.","수정1");
+
+        mockMvc.perform(patch("/posts/{postId}",post.getId())
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(editPost)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("글 내용 삭제")
+    void 글_삭제() throws Exception {
+        Post post = new Post("제목1", "수정1");
+        postRepository.save(post);
+
+
+        mockMvc.perform(delete("/posts/{postId}",post.getId())
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 }
--- src/test/java/com/example/toyblog/service/PostServiceTest.java
+++ src/test/java/com/example/toyblog/service/PostServiceTest.java
@@ -2,6 +2,8 @@ package com.example.toyblog.service;
 
 import com.example.toyblog.domain.Post;
 import com.example.toyblog.dto.request.CreatePost;
+import com.example.toyblog.dto.request.EditPost;
+import com.example.toyblog.dto.request.SearchOption;
 import com.example.toyblog.dto.response.PostResponse;
 import com.example.toyblog.repository.PostRepository;
 import org.junit.jupiter.api.Assertions;
@@ -10,7 +12,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -61,6 +70,91 @@ class PostServiceTest {
         assertEquals("제목2",findPost.getTitle());
         assertEquals("내용2",findPost.getContent());
     }
+
+    @Test
+    @DisplayName("페이지 조회")
+    void 글_조회(){
+        List<Post> requestPosts = IntStream.range(0,20)
+                .mapToObj(i-> new Post("제목"+i,"내용"+i))
+                .collect(Collectors.toList());
+
+        postRepository.saveAll(requestPosts);
+
+        SearchOption option = new SearchOption(1,10);
+
+
+        List<PostResponse> posts = postService.getList(option);
+
+        assertEquals(10L,posts.size());
+        assertEquals("제목19",posts.get(0).getTitle());
+
+    }
+
+    @Test
+    @DisplayName("글 제목 수정")
+    void 글_제목_수정(){
+        Post post = new Post("제목입니다!", "내용입니다!");
+        postRepository.save(post);
+
+        EditPost changedPost = new EditPost("바뀐 제목입니다", "내용입니다!");
+        postService.edit(post.getId(),changedPost);
+
+        postRepository.findById(post.getId())
+                        .orElseThrow(()->new RuntimeException("글이 존재하지 않습니다. id=" + post.getId()));
+
+        assertEquals("바뀐 제목입니다",post.getTitle());
+
+    }
+
+    @Test
+    @DisplayName("글 내용 수정")
+    void 글_내용_수정(){
+        Post post = new Post("제목입니다!", "내용입니다!");
+        postRepository.save(post);
+
+        EditPost changedPost = new EditPost("제목입니다!", "바뀐 내용입니다.");
+        postService.edit(post.getId(),changedPost);
+
+        postRepository.findById(post.getId())
+                .orElseThrow(()->new RuntimeException("글이 존재하지 않습니다. id=" + post.getId()));
+
+        assertEquals("바뀐 내용입니다.",post.getContent());
+
+    }
+
+    @Test
+    @DisplayName("글 내용 수정")
+    void 글_내용_수정_ver2(){
+        Post post = new Post("제목입니다!", "내용입니다!");
+        postRepository.save(post);
+
+        EditPost changedPost = new EditPost(null, "바뀐 내용입니다.");
+        postService.edit(post.getId(),changedPost);
+
+        postRepository.findById(post.getId())
+                .orElseThrow(()->new RuntimeException("글이 존재하지 않습니다. id=" + post.getId()));
+
+        assertEquals("제목입니다!",post.getTitle());
+        assertEquals("바뀐 내용입니다.",post.getContent());
+
+    }
+
+    @Test
+    @DisplayName("글 내용 수정")
+    void 글_삭제(){
+        Post post = new Post("제목입니다!", "내용입니다!");
+        postRepository.save(post);
+
+        postRepository.findById(post.getId())
+                .orElseThrow(()->new RuntimeException("글이 존재하지 않습니다. id=" + post.getId()));
+
+        postService.delete(post.getId());
+
+        assertEquals(0,postRepository.count());
+
+    }
+
+
 
 
 
