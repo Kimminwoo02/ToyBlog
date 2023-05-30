7fecf5237b7bb5293d62993cae6e0f2d58b5d305
minu <made_power@naver.com>
2023-05-30 14:31:35 +0900

    에러 처리를 위한 ExceptionController, Response 생성
    데이터 기반으로 통신하기 위해 Error 전용 컨트롤러를 @ControllerAdvice를 이용하여 생성하였다. 클라이언트에서 오류에 관한 코드와 메시지를 확인할 수 있도록 필드를 생성하였다.

---
+++ src/main/java/com/example/toyblog/controller/ExceptionController.java
@@ -0,0 +1,38 @@
+package com.example.toyblog.controller;
+
+import com.example.toyblog.dto.request.CreatePost;
+import com.example.toyblog.dto.response.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e){
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String message = fieldError.getDefaultMessage();
+
+        ErrorResponse response = new ErrorResponse("400","잘못된 요청입니다.");
+
+
+        response.addValidation(fieldError.getField(),fieldError.getDefaultMessage());
+
+        return response;
+    }
+
+}
