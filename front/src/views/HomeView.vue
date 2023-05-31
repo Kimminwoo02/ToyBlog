f84c89fcbf4a36ddd98cb6575346988a9ffbd088
minu <made_power@naver.com>
2023-06-01 01:15:38 +0900

    홈 화면 디자인 및 글 조회 기능 구현
    기본적인 디자인과 글을 조회하는 기능을 구현하였다.

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,35 +1,71 @@
 <script setup lang="ts">
 import axios from 'axios';
 import {ref} from "vue";
-const posts = ref([])
-axios.get("api/posts?page=1&size=5").then((response)=>{
-  response.data.forEach((r:any)=>{
-    posts.value.push(r);
-  })
 
-})
+const posts = ref([])
+
+axios.get("api/posts?page=1&size=5").then((response) => {
+  response.data.forEach((r: any) => {
+    posts.value.push(r);
+  });
+});
 
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.id">
-      <div>
-        {{post.title}}
+      <div class="title">
+        <router-link :to="{name:'read',params:{ postId : post.id}}"> {{ post.title }}</router-link>
       </div>
-      <div>
-        {{post.content}}
+
+      <div class="content">
+        {{ post.content }}
+      </div>
+      <div class="sub d-flex">
+        <div class="category">개발</div>
+        <div class="regDate">2023-03-03</div>
       </div>
     </li>
   </ul>
 
 </template>
-<style scoped>
-li{
-  margin-bottom: 1rem;
+<style scoped lang="scss">
+ul{
+  list-style: none;
+  padding: 0;
+  li {
+    margin-bottom: 1.4rem;
+    .title {
+      a{
+        font-size:1.2rem;
+        color: #383838;
+        text-decoration: none;
+      }
+      &:hover{
+        text-decoration: underline;
+      }
+    }
+    .content{
+      font-size: 0.95rem;
+      margin-top: 8px;
+      color: #5d5d5d;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+    .sub{
+      margin-top: 8px;
+      font-size: 0.8rem;
+
+      .regDate{
+        margin-left: 5px;
+        color: #6b6b6b;
+      }
+    }
+  }
 }
-li:last-child{
-  margin-bottom: 0;
-}
+
+
 
 </style>
