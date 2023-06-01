1f2f73eff0d503ac55880c77100bad84c8e1984f
minu <made_power@naver.com>
2023-06-01 21:11:07 +0900

    유저 Repository 및 AuthService
    커밋에 빠진 코드를 넣어주기 위한 커밋

---
+++ src/main/java/com/example/toyblog/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.example.toyblog.service;
+
+import com.example.toyblog.domain.Session;
+import com.example.toyblog.domain.User;
+import com.example.toyblog.dto.request.Login;
+import com.example.toyblog.exception.LoginError;
+import com.example.toyblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public String signIn(Login login){
+        User user = userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword())
+                .orElseThrow(LoginError::new);
+        Session session = user.addSession();
+        return session.getAccessToken();
+    }
+
+}
