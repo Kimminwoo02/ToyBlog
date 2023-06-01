2e70dacdf615636ced7e0f1a74c41e8e82a01c4d
minu <made_power@naver.com>
2023-06-01 21:09:48 +0900

    DB 연결
    Mysql을 사용하도록 했다. 초기 데이터로 2개만 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user` (name, email,password,created_at) VALUES('미누11','kmw1111950701@naver.com','1234','2023-06-01')
+INSERT INTO `user` (name, email,password,created_at) VALUES('미누2','krgrgee','12342','2023-06-013')
+
+select * from user
