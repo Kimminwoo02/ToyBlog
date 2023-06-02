7521a13c3097a9286a5558eae45d6df199e693b9
minu <made_power@naver.com>
2023-06-02 15:58:37 +0900

    공통으로 사용하느 소스 추가
    보안상으로 완벽하진 않지만 application.yml에 공통 소스를 넣고 사용

--- src/main/java/com/example/toyblog/ToyBlogApplication.java
+++ src/main/java/com/example/toyblog/ToyBlogApplication.java
@@ -1,8 +1,12 @@
 package com.example.toyblog;
 
+import com.example.toyblog.config.AppConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppConfig.class)
 @SpringBootApplication
 public class ToyBlogApplication {
 
