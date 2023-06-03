de4203bf2a1d2b8b9f138e8a3a87c9bd0edde1e5
minu <made_power@naver.com>
2023-06-04 02:24:44 +0900

    로그인 여부에 따른 헤더출력 화면

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -3,13 +3,19 @@
     <el-menu mode="horizontal" router>
       <el-menu-item index="/">Home</el-menu-item>
       <el-menu-item index="/write">글 작성</el-menu-item>
-      <el-menu-item index="/logIn">로그인</el-menu-item>
+      <el-menu-item index="/home" v-if="cookie">로그아웃</el-menu-item>
+      <el-menu-item index="/logIn" v-else>로그인</el-menu-item>
       <el-menu-item index="/signIn">회원가입</el-menu-item>
+
     </el-menu>
   </el-header>
 </template>
 
-<script setup lang ="ts"></script>
+<script setup lang ="ts">
+import {computed} from "vue";
+
+
+</script>
 
 <style scoped>
 .header{
